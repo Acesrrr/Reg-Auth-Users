99ed5a2e403a9043d3df40951e058619dcf76db2
Acesrrr <34470048+Acesrrr@users.noreply.github.com>
2017-12-15 15:40:01 +0300

    Add files via upload

--- views/index/view.php
+++ views/index/view.php
@@ -1,6 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-	<?php include_once SITE_PATH."views/header/view.php";?>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<title><?=SITE_NAME;?></title>
+		<!----стили, jquery и ajax----->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link href="/css/style.css" rel="stylesheet">
+		<script type="text/javascript">
+			$(document).ready(function ()
+
+			{
+				//Установление направления отображения popover
+				$('[data-toggle="popover"]').popover({
+					placement : 'bottom'
+				});
+	  
+			});
+		</script>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="/js/reg.js"></script>
+		
+		
+
+		
+	</head>
 	<body>
 		<div class="container">
 			<ul class="nav nav-tabs">
--- views/registration/view.php
+++ views/registration/view.php
@@ -1,6 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-	<?php include_once SITE_PATH."views/header/view.php";?>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<title><?=SITE_NAME;?></title>
+		<!----стили, jquery и ajax----->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link href="/css/style.css" rel="stylesheet">
+		
+		<script type="text/javascript">
+			$(document).ready(function ()
+
+			{
+				//Установление направления отображения popover
+				$('[data-toggle="popover"]').popover({
+					placement : 'bottom'
+				});
+	  
+			});
+		</script>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="/js/reg.js"></script>
+		<script type="text/javascript" src="/js/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="/js/jquery.form.js"></script>
+	</head>
 	<body>
 		<div class="container">
 			<ul class="nav nav-tabs">
@@ -17,8 +44,6 @@
 					<p align="center" id="reg_message_rus"></p>
 					<p class="col-12 col-sm-12 col-md-12 col-lg-12" align="center" id="reg_return_rus"><a href="index.php">Вернуться на главную</a></p>
 					<form enctype="multipart/form-data" method="post" id="form_reg_rus" action="/ajax/handler_reg.php">
-					
-					<p class="col-12 col-sm-12 col-md-12 col-lg-12" align="center" id="reg_return_rus"><a href="index.php">Вернуться на главную</a></p>
 						<div id="block_registration_rus">
 							<ul style="list-style-type:none;" class="form_reg_rus">
 								<li>
@@ -91,7 +116,10 @@
 									<div class="form-group row">
 										<label for="user_image_rus" class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 col-form-label">Изображение(фото)</label>
 										<div class="col-12 col-sm-12 col-md-8 col-lg-6 col-xl-6">
-											<input type="file" class="form-control-file" multiple accept="image/jpeg,image/png" name="user_image" id="user_image" placeholder="Добавьте фото">
+										
+											  <input type="file" multiple accept="image/jpeg,image/png" name="user_image" id="user_image">
+											 
+											
 											<small  class="form-text text-muted">
 												Добавьте изображение.
 											</small>
@@ -108,7 +136,6 @@
 					<p align="center" id="reg_message_eng"></p>
 					<p class="col-12 col-sm-12 col-md-12 col-lg-12" align="center" id="reg_return_eng"><a href="index.php">Back home</a></p>
 					<form enctype="multipart/form-data" method="post" id="form_reg_eng" action="/ajax/handler_reg.php">
-						<p class="col-12 col-sm-12 col-md-12 col-lg-12" align="center" id="reg_return_eng"><a href="index.php">Back home</a></p>
 						<div id="block_registration_eng">
 							<ul style="list-style-type:none;" class="form_reg_eng">
 								<li>
@@ -182,7 +209,10 @@
 									<div class="form-group row">
 										<label for="user_image_eng" class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 col-form-label">Image(photo)</label>
 										<div class="col-12 col-sm-12 col-md-8 col-lg-6 col-xl-6">
-											<input type="file" class="form-control-file" multiple accept="image/jpeg,image/png" name="user_image" id="user_image" placeholder="Place your photo,please">
+											<label class="custom-file">
+											  <input type="file" multiple accept="image/jpeg,image/png" name="user_image" id="user_image" class="custom-file-input">
+											  <span class="custom-file-control"></span>
+											</label>
 											<small class="form-text text-muted">
 												Please,put your image.
 											</small>
